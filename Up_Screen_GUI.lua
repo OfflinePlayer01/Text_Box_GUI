957688b975826179790604ecd8a586e3aad56480
OfflinePlayer01 <168975740+OfflinePlayer01@users.noreply.github.com>
2024-05-29 20:59:39 +0300

    Up_Screen_GUI.lua

--- Up_Screen_GUI.lua
+++ Up_Screen_GUI.lua
@@ -2,12 +2,12 @@ local screenGui = Instance.new("ScreenGui")
 screenGui.Parent = game.Players.LocalPlayer.PlayerGui
 
 local textBox = Instance.new("TextBox")
-textBox.Size = UDim2.new(1, 1, -5, 1) -- Set the size to fill the entire screen
-textBox.Position = UDim2.new(0, 0, 0, 0) -- Set the position to the top-left corner
-textBox.BackgroundColor3 = Color3.new(1, 1, 1) -- Set the background color to white
+textBox.Size = UDim2.new(1, 1, -5, 1)
+textBox.Position = UDim2.new(0, 0, 0, 0)
+textBox.BackgroundColor3 = Color3.new(0, 0, 0)
 textBox.BorderSizePixel = 0
-textBox.BackgroundTransparency = 0.7
-textBox.FontSize = Enum.FontSize.Size14 -- Set the font size to 14
+textBox.BackgroundTransparency = 0
+textBox.FontSize = Enum.FontSize.Size14
 textBox.Parent = screenGui
 
 local timer = 5
